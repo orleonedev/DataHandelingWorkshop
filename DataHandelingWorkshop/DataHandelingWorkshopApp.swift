a0b3f1505d3d8805f1cdb3438a428bca50720d98
orleonedev <orleone.dev@gmail.com>
2021-11-17 09:35:56 +0100

    Initial Commit

---
+++ DataHandelingWorkshop/DataHandelingWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataHandelingWorkshopApp.swift
+//  DataHandelingWorkshop
+//
+//  Created by Oreste Leone on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DataHandelingWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
